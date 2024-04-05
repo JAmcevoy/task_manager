b2e78157d9410b2d452f523a666cc38bf7ebd403
Joshua McEvoy <joshmcevoy18@gmail.com>
2024-04-05 10:34:04 +0000

    Viewing Tasks - complete

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,22 +1,20 @@
-// This event listener listens for the DOMContentLoaded event, which indicates that the initial HTML document has been completely loaded and parsed, including stylesheets, images, and subframes.
-
 document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
-    // Select all elements with the class "sidenav" and initialize them as Materialize sidenav components.
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
     // datepicker initialization
-    // Select all elements with the class "datepicker" and initialize them as Materialize datepicker components.
-    // The options object passed to M.Datepicker.init() specifies the format of the date and customizes the "done" button text.
     let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
-        format: "dd mmmm, yyyy", // Date format: day, month (full name), year
-        i18n: {done: "Select"}    // Customized "done" button text
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
     });
 
     // select initialization
-    // Select all <select> elements and initialize them as Materialize select components.
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
+
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
 });
